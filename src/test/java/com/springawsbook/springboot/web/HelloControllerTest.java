4aabdf545970a6d75fc35de6918de3d71afb6429
physicksKim <physicksKim@gmail.com>
2022-12-27 23:42:53 +0900

    add hello controller and Hello Test code

---
+++ src/test/java/com/springawsbook/springboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.springawsbook.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(SpringExtension.class) // junit5 에서 스프링부트 테스트를 할 수 있도록 넣어줌
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc; // 웹 MVC 테스트 할 때 사용함
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
