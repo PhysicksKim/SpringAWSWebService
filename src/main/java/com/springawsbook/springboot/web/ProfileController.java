ef9aae03e847e68a85ae0308deb37a1f4df7666f
physicksKim <physicksKim@gmail.com>
2023-01-07 18:57:09 +0900

    updated nohup jar option in scripts/deploy.sh

--- src/main/java/com/springawsbook/springboot/web/ProfileController.java
+++ src/main/java/com/springawsbook/springboot/web/ProfileController.java
@@ -1,6 +1,7 @@
 package com.springawsbook.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
